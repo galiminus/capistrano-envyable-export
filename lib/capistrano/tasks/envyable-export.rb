03b5d664b04bfc1c893ab34b51e79fce76bee6ba
Victor Goya <victor.goya@precogs.fr>
2015-05-21 13:22:17 +0200

    add support for the regular env.yml file

--- lib/capistrano/tasks/envyable-export.rb
+++ lib/capistrano/tasks/envyable-export.rb
@@ -1,25 +1,29 @@
 require 'pathname'
 require 'envyable'
+require 'yaml'
 
 namespace :envyable do
   desc 'Export the .env file to the remote servers'
   task :export do
     on roles fetch(:envyable_roles) do
-      execute(:mkdir, "-p", Pathname.new(fetch(:envyable_export_path)).dirname)
-
       env = {}.tap do |env|
-        Envyable::Loader.new(fetch(:envyable_local_path), env).load(fetch(:stage).to_s)
-      end.map { |k, v| "#{k}=#{v}" }.join("\n")
+        Envyable::Loader.new(fetch(:envyable_path), env).load(fetch(:stage).to_s)
+      end
 
-      upload! StringIO.new(env), fetch(:envyable_export_path, -> { fetch(:envyable_export_path) })
+      upload! StringIO.new(YAML.dump(env)), "#{release_path}/#{fetch(:envyable_path)}"
+      if fetch(:envyable_env_path)
+        execute(:mkdir, "-p", Pathname.new(fetch(:envyable_env_path)).dirname)
+        upload! StringIO.new(env.map { |k, v| "#{k}=#{v}" }.join("\n")), fetch(:envyable_env_path)
+      end
     end
   end
+
+  before 'deploy:publishing', 'envyable:export'
 end
 
 namespace :load do
   task :defaults do
     set :envyable_roles, fetch(:envyable_roles, :all)
-    set :envyable_local_path, fetch(:envyable_local_path, "config/env.yml")
-    set :envyable_export_path, ".env"
+    set :envyable_path, fetch(:envyable_path, "config/env.yml")
   end
 end
